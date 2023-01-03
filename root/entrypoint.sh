da10f9517148089153c3ee6617f50cbf80e64e66
sevenrats <sevenrats@tutanota.com>
2023-01-03 02:07:21 -0500

    initial commit

---
+++ root/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Proxy signals
+sp_processes=("electrum-nmc" "ncdns" "coredns")
+. /signalproxy.sh
+
+set -ex
+
+# Overload Traps
+  #none
+
+# Configure Stuff
+electrum-nmc $FLAGS --offline setconfig rpcuser ${ELECTRUM_USER}
+electrum-nmc $FLAGS --offline setconfig rpcpassword ${ELECTRUM_PASSWORD}
+electrum-nmc $FLAGS --offline setconfig rpchost 0.0.0.0
+electrum-nmc $FLAGS --offline setconfig rpcport 7000
+
+# Run application
+electrum-nmc $FLAGS daemon & \
+ncdns -conf /data/ncdns/ncdns.conf & \
+coredns -conf /data/coredns/corefile & \
+wait -n
