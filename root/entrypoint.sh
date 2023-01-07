7f61c76fbd16379368e3988144bea83daf5f3bf7
sevenrats <sevenrats@tutanota.com>
2023-01-07 15:37:33 -0500

    remove experimental healthchecks from release code

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -21,30 +21,8 @@ do
   fi
 done
 
-# Define Healthcheck Functions
-_nmc_core_is_up () {
-    return 1
-}
-_health_namecoind () {
-  echo "We are doing this shit"
-  return 0
-}
-_health_coredns () {
-  curl -s http://localhost:1053/health
-}
 # Run application
 namecoind -conf=/data/namecoin-core/namecoin.conf >/dev/null & \
 ncdns -conf /data/ncdns/ncdns.conf & \
 coredns -conf /data/coredns/corefile & \
-# HealthCheck
-until _nmc_core_is_up
-do
-    sleep 15
-    if namecoin-cli getblockchaininfo | grep -q '"initialblockdownload": true'; then
-      echo "Namecoin-Core is still syncing with the blockchain."
-    fi
-    _health_namecoind
-    _health_ncdns
-    _health_coredns
-done & \
 wait -n
